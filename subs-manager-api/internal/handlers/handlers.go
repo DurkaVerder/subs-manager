bd2e983060dedd0dc1def4fe0864b458d6d09449
DurlaVerder <vrrrr227@gmail.com>
2025-07-16 13:33:29 +0300

    Added handlers for get subcribes

--- subs-manager-api/internal/handlers/handlers.go
+++ subs-manager-api/internal/handlers/handlers.go
@@ -1,6 +1,17 @@
 package handlers
 
-import "subs-manager-api/internal/models"
+import (
+	"fmt"
+	"net/http"
+	"subs-manager-api/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	ServiceNameQuery = "service-name"
+	UserIDQuery      = "user-id"
+)
 
 type SubscribeService interface {
 	GetSubscriptionFilter(data models.DataFilter) ([]models.ServiceSubscriptionResponse, error)
@@ -18,4 +29,41 @@ type Handler struct {
 
 func NewHandler(subscribeService SubscribeService) *Handler {
 	return &Handler{subscribeService: subscribeService}
+}
+
+func (h *Handler) GetSubscriptionHandler(ctx *gin.Context) {
+	serviceName := ctx.Query(ServiceNameQuery)
+	userID := ctx.Query(UserIDQuery)
+
+	if serviceName == "" || userID == "" {
+		ctx.JSON(http.StatusBadRequest, fmt.Sprintf("serviceName = %s, userID = %s", serviceName, userID))
+	}
+
+	response, err := h.subscribeService.GetSubscription(serviceName, userID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"subscribe": response,
+	})
+
+}
+
+func (h *Handler) GetSubscriptionsHandler(ctx *gin.Context) {
+	userID := ctx.Query(UserIDQuery)
+
+	if userID == "" {
+		ctx.JSON(http.StatusBadRequest, "invalid userID")
+	}
+
+	response, err := h.subscribeService.GetSubscriptions(userID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"subscribes": response,
+	})
+
 }
